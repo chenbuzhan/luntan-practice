ffa35672a3011f587321d40ee8d5c55fa3b24ea7
unknown <281952881@qq.com>
2020-03-15 11:12:47 +0800

    init repo

---
+++ src/test/java/buzhan/practice/demo/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package buzhan.practice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
